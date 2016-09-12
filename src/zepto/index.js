682b76a0470ed8e86ad68ac7f7ecb586a1e1f81f
weikang.zhuwk <weikang.zhuwk@alibaba-inc.com>
2016-09-12 22:54:58 +0800

    使用新的写法

--- src/zepto/index.js
+++ src/zepto/index.js
@@ -1668,68 +1668,92 @@ window.$ === undefined && (window.$ = Zepto)
 })(Zepto)
 
 // 扩展模板
-;(function($){
+;(function($) {
   // underscore tmpl
+  var templateSettings = {
+    evaluate: /<%([\s\S]+?)%>/g,
+    interpolate: /<%=([\s\S]+?)%>/g,
+    escape: /<%-([\s\S]+?)%>/g
+  };
 
-  var tmpl = function( text, data ){
-    var settings, render, noMatch, matcher, index, source, escaper, escapes, template;
+  // When customizing `templateSettings`, if you don't want to define an
+  // interpolation, evaluation or escaping regex, we need one that is
+  // guaranteed not to match.
+  var noMatch = /(.)^/;
 
-    settings = { evaluate: /<%([\s\S]+?)%>/g, interpolate: /<%=([\s\S]+?)%>/g };
-    noMatch = /(.)^/;
+  // Certain characters need to be escaped so that they can be put into a
+  // string literal.
+  var escapes = {
+    "'": "'",
+    '\\': '\\',
+    '\r': 'r',
+    '\n': 'n',
+    '\u2028': 'u2028',
+    '\u2029': 'u2029'
+  };
 
-    matcher = new RegExp( [
-        ( settings.interpolate || noMatch ).source,
-        ( settings.evaluate || noMatch ).source
-    ].join('|') + '|$', 'g' );
+  var escapeRegExp = /\\|'|\r|\n|\u2028|\u2029/g;
 
-    index = 0;
-    source = "__p+='";
-    escaper = /\\|'|\r|\n|\t|\u2028|\u2029/g;
-    escapes = {
-        "'": "'", '\\': '\\', '\r': 'r', '\n': 'n', '\t': 't', '\u2028': 'u2028',
-        '\u2029': 'u2029'
-    };
+  var escapeChar = function(match) {
+    return '\\' + escapes[match];
+  };
 
-    text.replace( matcher, function( match, interpolate, evaluate, offset ){
-        source += text.slice( index, offset ).replace( escaper, function( match ){
-            return '\\' + escapes[ match ];
-        } );
+  var tmpl = function(text, settings, oldSettings) {
+    if (!settings && oldSettings) settings = oldSettings;
+    settings = _.defaults({}, settings, templateSettings);
 
-        if( interpolate )
-            source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+    // Combine delimiters into one regular expression via alternation.
+    var matcher = RegExp([
+      (settings.escape || noMatch).source,
+      (settings.interpolate || noMatch).source,
+      (settings.evaluate || noMatch).source
+    ].join('|') + '|$', 'g');
 
-        if( evaluate )
-            source += "';\n" + evaluate + "\n__p+='";
+    // Compile the template source, escaping string literals appropriately.
+    var index = 0;
+    var source = "__p+='";
+    text.replace(matcher, function(match, escape, interpolate, evaluate, offset) {
+      source += text.slice(index, offset).replace(escapeRegExp, escapeChar);
+      index = offset + match.length;
 
-        index = offset + match.length;
-        return match;
-    } );
+      if (escape) {
+        source += "'+\n((__t=(" + escape + "))==null?'':_.escape(__t))+\n'";
+      } else if (interpolate) {
+        source += "'+\n((__t=(" + interpolate + "))==null?'':__t)+\n'";
+      } else if (evaluate) {
+        source += "';\n" + evaluate + "\n__p+='";
+      }
 
+      // Adobe VMs need the match returned to produce the correct offset.
+      return match;
+    });
     source += "';\n";
-    source = 'with(obj||{}){\n' + source + '}\n';
-    source = "var __t,__p='',__j=Array.prototype.join," +
-        "print=function(){__p+=__j.call(arguments,'');};\n" + source + "return __p;\n";
 
-    try{
-        render = new Function( 'obj', source );
-    }catch( e ){
-        e.source = source;
-        throw e;
+    // If a variable is not specified, place data values in local scope.
+    if (!settings.variable) source = 'with(obj||{}){\n' + source + '}\n';
+
+    source = "var __t,__p='',__j=Array.prototype.join," +
+      "print=function(){__p+=__j.call(arguments,'');};\n" +
+      source + 'return __p;\n';
+
+    var render;
+    try {
+      render = new Function(settings.variable || 'obj', '_', source);
+    } catch (e) {
+      e.source = source;
+      throw e;
     }
 
-    if( data )
-        return render( data );
-
-    template = function( data ) {
-        return render.call( this, data );
+    var template = function(data) {
+      return render.call(this, data, _);
     };
 
-    template.source = 'function(obj){\n' + source + '}';
+    // Provide the compiled source as a convenience for precompilation.
+    var argument = settings.variable || 'obj';
+    template.source = 'function(' + argument + '){\n' + source + '}';
 
     return template;
   }
   $.tmpl = tmpl;
-
 })(Zepto)
-
 module.exports = Zepto
